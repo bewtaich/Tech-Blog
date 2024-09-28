0c0f6ece66f3e9b3dd00b21d0e83d06fdaed39d9
Bewtaich <bewtaich@gmail.com>
2024-09-28 17:58:12 -0500

    connected routes to export

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -21,7 +21,7 @@ router.post('/signup', async (req, res) => {
 
 router.post('/login', async (req, res) => {
     try {
-      const userData = await User.findOne({ where: { email: req.body.email } });
+      const userData = await User.findOne({ where: { username: req.body.username } });
   
       if (!userData) {
         res
