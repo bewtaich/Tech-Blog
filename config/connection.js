991c030559c91316c6243c5f9f1f1b0c619de77c
Bewtaich <bewtaich@gmail.com>
2024-09-30 16:15:36 -0500

    connection changed for deployment

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,25 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,  
+          rejectUnauthorized: false,  
+        },
+      },
+    })
   : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
+      process.env.DB_NAME,      
+      process.env.DB_USER,      
+      process.env.DB_PASSWORD, 
       {
-        host: "localhost",
-        dialect: "postgres",
+        host: "localhost",      
+        dialect: "postgres",    
       }
     );
 
 module.exports = sequelize;
+
